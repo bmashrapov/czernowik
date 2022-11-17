66354831a05b7fe851f162237248ae1d2fbf6498
b.Mashrapov <b_mashrapov@mail.ru>
2022-11-17 11:57:30 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -45,16 +46,17 @@ public class Main {
 
         System.out.println("Задача 4.4");
         for (int i = 1; i <= 30; i++) {
-                if (i % 3 == 0) {
-                    System.out.println(i + ":ping");
-                }
-                if (i % 5 == 0) {
-                    System.out.println(i + ":pong");
-                }
-                if (i % 3 == 0 && i % 5 == 0) {
-                    System.out.println(i + ":ping pong");}
-
-                else {System.out.println(i + ":");}
+            if (i % 3 == 0) {
+                System.out.println(i + ":ping");
+            }
+            if (i % 5 == 0) {
+                System.out.println(i + ":pong");
+            }
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + ":ping pong");
+            } else {
+                System.out.println(i + ":");
+            }
         }
         System.out.println("Задача 5.5");
         int[][] array = new int[3][3];
@@ -74,30 +76,76 @@ public class Main {
             System.out.println();
         }
         System.out.println("Задача 5.6");
-        int [] f = {5,4,3,2,1};
+        int[] f = {5, 4, 3, 2, 1};
         for (int i = 0; i < f.length; i++) {
             System.out.print(f[i] + " ");
-        }System.out.println();
-        for (int i = f.length-1; i >= 0 ; i--) {
-            System.out.print( f[i] + " ");
-            }System.out.println();
+        }
+        System.out.println();
+        for (int i = f.length - 1; i >= 0; i--) {
+            System.out.print(f[i] + " ");
+        }
+        System.out.println();
         System.out.println("Задача 5.7");
-        int [] arr = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        int[] arr = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (arr[i] + arr[j] == -2){
+                if (arr[i] + arr[j] == -2) {
                     System.out.println(arr[i] + " " + arr[j]);
                 }
             }
         }
-        System.out.println("Задача 6.5");
-        String fullName = " Ivanov Ivan Ivanovich";
-        String[] words = fullName.split(" ");
-        String fN = words[2];
-        String mN = words[3];
-        String lN = words[1];
+        System.out.println("Задача 6.5, 6.6");
+        String fullName = " ivanov ivan ivanovich";
+        String upperCaseLine = "";
+        Scanner lineScan = new Scanner(fullName);
+        while (lineScan.hasNext()) {
+            String word = lineScan.next();
+            upperCaseLine += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
+        }
+        System.out.println("ФИО " + upperCaseLine);
+        String[] words = upperCaseLine.split(" ");
+        String fN = words[1];
+        String mN = words[2];
+        String lN = words[0];
         System.out.println("Имя сотрудника - " + fN);
         System.out.println("Фамилия сотрудника - " + lN);
         System.out.println("Отчество сотрудника - " + mN);
+        System.out.println("Задача 6,7");
+        String numbers1 = "135";
+        StringBuilder numbers2 = new StringBuilder("246");
+        numbers2.append(numbers1);
+        numbers2.setCharAt(0, '1');
+        numbers2.setCharAt(1, '2');
+        numbers2.setCharAt(2, '3');
+        numbers2.setCharAt(3, '4');
+        numbers2.setCharAt(4, '5');
+        numbers2.setCharAt(5, '5');
+        System.out.println(numbers2);
+        System.out.println("Задача 6.7 вариант 2");
+        String firstString = "135";
+        String secondString = "246";
+        String unitedString;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < firstString.length(); i++) {
+            sb.append(firstString.charAt(i)).append(secondString.charAt(i));
+        }
+        unitedString = sb.toString();
+        System.out.println(unitedString);
+        System.out.println("Задача 6,8");
+        String sortSymbols = "aabccddefgghiijjkk";
+        String sortDublicatedSymbols;
+        char [] symbolsArray = sortSymbols.toCharArray();
+        StringBuilder sb1 = new StringBuilder();
+        char dublicated = ' ';
+        for (int i = 0; i < symbolsArray.length; i++) {
+            if (symbolsArray[i] == dublicated) {
+                sb1.append(symbolsArray[i]);
+            }
+            dublicated = symbolsArray[i];
+        }
+        sortDublicatedSymbols = sb1.toString();
+        System.out.println(sortDublicatedSymbols);
+
+
     }
 }
